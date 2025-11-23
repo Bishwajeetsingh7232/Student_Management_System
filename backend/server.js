eb0d4b2e43224206c37cb35179069e25c3ef9cb3
Bishwajeetsingh7232 <singhbishwa7232@gmail.com>
2025-11-23 14:45:10 +0530

    Added PORT env support

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(cors());
